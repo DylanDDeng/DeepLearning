f43dd0de80777b82943afebaed444e76d8b0729d
ChengshengDeng <chengshengdeng0308@163.com>
2023-04-19 09:56:52 +0800

    update code and comments

--- ResNet/model.py
+++ ResNet/model.py
@@ -34,7 +34,7 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         identity = x
-        if self.downsample is not None:
+        if self.downsample is not None:  # 判断downsample是否为None， 如果为None的话对应的是实线的残差结构， 如果不为None的话对应的是虚线的残差结构
             identity = self.downsample(x)
 
         out = self.conv1(x)
@@ -50,48 +50,57 @@ class BasicBlock(nn.Module):
         return out
 
 # ResNet50 ,ResNet101, ResNet152
-    class Bottleneck(nn.Module):
-        expansion = 4
-        def __init__(self, in_channel, out_channel, stride=1, downsample=None, groups=1,
-                     width_per_group=64):
-            super().__init__()
-            width = int(out_channel * (width_per_group / 64.)) * groups
-            self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width, kernel_size=1, stride=1, bias=False)
-            self.bn1 = nn.BatchNorm2d(width)
-            self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups, kernel_size=3, stride=stride, bias=False,
-                                   padding=1)
-            self.bn2 = nn.BatchNorm2d(width)
-            self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion, kernel_size=1, stirde=1, bias=False)
-            self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
-            self.relu = nn.ReLU(inplace=True)
-            self.downsample = downsample
+class Bottleneck(nn.Module):
+    expansion = 4
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, groups=1,
+                width_per_group=64):
+        super(Bottleneck, self).__init__()
+        width = int(out_channel * (width_per_group / 64.)) * groups
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=width, kernel_size=1, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(width)
+        self.conv2 = nn.Conv2d(in_channels=width, out_channels=width, groups=groups, kernel_size=3, stride=stride, bias=False,
+                                padding=1)
+        self.bn2 = nn.BatchNorm2d(width)
+        self.conv3 = nn.Conv2d(in_channels=width, out_channels=out_channel*self.expansion, kernel_size=1, stirde=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
 
-        def forward(self, x):
-            identity = x
-            if self.downsample is not None:
-                identity = self.downsample(x)
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:  # 判断downsample是否为None， 如果为None的话对应的是实线的残差结构， 如果不为None的话对应的是虚线的残差结构
+            identity = self.downsample(x)
 
-            out = self.conv1(x)
-            out = self.bn1(out)
-            out = self.relu(out)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-            out = self.conv2(out)
-            out = self.bn2(out)
-            out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
 
-            out = self.conv3(out)
-            out = self.bn3(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
 
-            out += identity
-            out = self.relu(out)
+        out += identity
+        out = self.relu(out)
 
-            return out
+        return out
 
 class ResNet(nn.Module):
+    r"""ResNet
+    Args:
+        block: (class): BasicBlock or Bottleneck
+        block_num: (list): number of residual block. For example: ResNet34, block_num = [3, 4, 6, 3]
+        num_classes:(int): number of classification
+        include_top:(boolean): Default = True
+        groups:(int): Default = 1
+        width_per_group:(int): Default = 64
+    """
     def __init__(self, block, block_num, num_classes=1000, include_top=True, groups=1, width_per_group=64):
         super(ResNet, self).__init__()
         self.include_top = include_top
-        self.in_channel = 64
+        self.in_channel = 64  # 通过 max pooling 层后的深度
 
         self.groups = groups
         self.width_per_group = width_per_group
@@ -101,10 +110,10 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(self.in_channel)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, block_num[0])
-        self.layer2 = self._make_layer(block, 128, block_num[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, block_num[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, block_num[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, block_num[0])  # conv2_x
+        self.layer2 = self._make_layer(block, 128, block_num[1], stride=2)  # conv3_x
+        self.layer3 = self._make_layer(block, 256, block_num[2], stride=2)  # conv4_x
+        self.layer4 = self._make_layer(block, 512, block_num[3], stride=2)  # conv5_x
 
         if self.include_top:
             self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # output size = (1,1)
@@ -116,6 +125,14 @@ class ResNet(nn.Module):
 
 
     def _make_layer(self, block, channel, block_num, stride=1):
+        """
+        :param block:  (class) BasicBlock or Bottleneck
+        :param channel: the number of kernel in the first layer of residual block
+                        eg: for the ResNet34, the channel is 64, 128, 256, 512
+        :param block_num: the number of residual block
+        :param stride: default = 1
+        :return:
+        """
         downsample = None
         if stride != 1 or self.in_channel != channel * block.expansion:
             downsample = nn.Sequential(
@@ -125,7 +142,7 @@ class ResNet(nn.Module):
 
         layers = []
         layers.append(block(self.in_channel, channel, downsample=downsample, stride=stride, groups=self.groups, width_per_group=self.width_per_group))
-        self.in_channel = channel.block.expansion
+        self.in_channel = channel * block.expansion
 
         for _ in range(1, block_num):
             layers.append(block(self.in_channel,
