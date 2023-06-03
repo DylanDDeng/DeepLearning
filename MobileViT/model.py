e3b78dae2e7e96f7c7d519e5ef94824652ce8565
ChengshengDeng <chengshengdeng0308@163.com>
2023-06-03 22:53:27 +0800

    MobileViT model

---
+++ MobileViT/model.py
@@ -0,0 +1,9 @@
+# -*- coding = utf-8 -*-  
+# @Time: 2023/6/3 22:52 
+# @Author: Dylan 
+# @File: model.py.py 
+# @software: PyCharm
+"""
+original code from apple:
+https://github.com/apple/ml-cvnets/blob/main/cvnets/models/classification/mobilevit.py
+"""
